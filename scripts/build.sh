62521cf1d78cc1761d7d42343a3dd73c186bb9c3
Husan Ibragimov <husandev@mail.ru>
2025-11-29 11:36:32 +0500

    Update Docker service port mappings for PostgreSQL, Redis, and RabbitMQ.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 echo "Building the project..."
 git pull origin main && echo "Project updated from the repository."
-cd docker/ && docker-compose --env-file ./../.envs/.env up --build -d
+cd docker/ && docker compose --env-file ./../.envs/.env up --build -d
 echo "Docker images built successfully."
 docker ps
