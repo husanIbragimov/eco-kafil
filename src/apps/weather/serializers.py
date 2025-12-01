f381b7aa073086b60cc72d9c254484edce9080e5
Husan Ibragimov <husandev@mail.ru>
2025-12-01 13:53:22 +0500

    Add pollutant field to WeatherSerializer with AQI classification

--- src/apps/weather/serializers.py
+++ src/apps/weather/serializers.py
@@ -6,6 +6,7 @@ from apps.weather.models import Weather
 class WeatherSerializer(serializers.ModelSerializer):
     region_code = serializers.CharField(source='station.code', read_only=True)
     region_name = serializers.CharField(source='station.name', read_only=True)
+    pollutant = serializers.SerializerMethodField()
 
     class Meta:
         model = Weather
@@ -22,5 +23,22 @@ class WeatherSerializer(serializers.ModelSerializer):
             "fog",
             "aqi",
             "date",
+            "pollutant",
         )
         read_only_fields = ("id", "date")
+
+    def get_pollutant(self, obj: Weather):
+        if obj.aqi > 0:
+            if obj.aqi < 50:
+                return "good"
+            elif 50 <= obj.aqi < 100:
+                return "moderate"
+            elif 100 <= obj.aqi < 150:
+                return "unhealthy for sensitive groups"
+            elif 150 <= obj.aqi < 200:
+                return "unhealthy"
+            elif 200 <= obj.aqi < 300:
+                return "very unhealthy"
+            else:
+                return "hazardous"
+        return "no data"
